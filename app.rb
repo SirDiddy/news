037a41787edbad60fac763c056dff4928202895a
SirDiddy <didunoluwaolamide@gmail.com>
2020-02-26 05:35:36 +0000

    Update

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ require 'news-api'
 def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }                                     
 
-# enter your Dark Sky API key here
+# Initialize Dark Sky API key here
 ForecastIO.api_key = "f1a2aea7e88a73ac70215f2b871e02cd"
 
 # Initialize News API
@@ -23,30 +23,36 @@ end
 get "/news" do
   # do everything else
 
-  puts "This is the related news"
     results = Geocoder.search(params["location"])
     @location = params["location"]
     lat_lng = results.first.coordinates
     puts results
     @lat = lat_lng[0]
     @lng = lat_lng[1]
-    puts @lat
-    puts @lng
+    
+    #shows the latitude
+    #puts @lat
+    
+    #shows the longitude
+    #puts @lng
 
+    #obtains the current weather 
     forecast = ForecastIO.forecast(@lat,@lng).to_hash
     @current_temp = forecast["currently"]["temperature"]
     @current_summary = forecast["currently"]["summary"]
 
+    #provides the weather forecast for the next couple of days
     forecast_array= forecast["daily"]["data"]
-    for forecasted_weather in  forecast_array
+    for forecasted_weather in forecast_array
         puts "A high temperature of #{forecasted_weather["temperatureHigh"]} and #{forecasted_weather["summary"]}"
     end 
 
+    
+  puts "This is the news"
     url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=48b7f2a840a24b23bea55b7ffd3f3f41"
-    news = HTTParty.get(url).parsed_response.to_hash
-    @articles = news["articles"]
-    #puts @news["articles"][3]
-
+    @news = HTTParty.get(url).parsed_response.to_hash
+    @articles = @news["articles"]
+    
     # news is now a Hash you can pretty print (pp) and parse for your output
     view "news"
 end
